2a52a4c0fee3f73ec53807ab7536a131036d14bc
Rachel4858 <rachel458458@gmail.com>
2020-06-20 23:31:33 +0900

    Connect mongoDB

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://blair:kong2kong2@@boilerplate-xabki.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: process.env.MOONGO_URI}
+
+  // 헤로쿠는 담에 다시 도전하기
