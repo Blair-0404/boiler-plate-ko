f2c83999e44f54dcce0aebf97b80b014473bdbfb
Rachel4858 <rachel458458@gmail.com>
2020-06-21 00:56:39 +0900

    Hash password using bcrypt

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 // node, expressJS 다운 후 expressJS 사용해서 간단한 어플 만들어봄
 
-const express = require('express') // express가져오기
-const app = express() // 함수이용해 새로운app만들고
-const port = 5000 // 어떤 숫자도 괜찮다. 나는 5000으로 설정
-const bodyParser = require('body-parser')
-const { User } = require("./models/User")
+const express = require('express'); // express가져오기
+const app = express(); // 함수이용해 새로운app만들고
+const port = 5000; // 어떤 숫자도 괜찮다. 나는 5000으로 설정
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 //설치한 몽구스를 이용해서 몽고디비와 연결하기
 
-const config = require('./config/key')
+const config = require('./config/key');
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI,{
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false}).then(() => console.log('몽고디비 연결성공'))
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false})
+  .then(() => console.log('몽고디비 연결성공'))
   .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('Hello World! 블레어입니다....')) // '/'인 즉 루트 디렉토리에 오면 헬로월드 출력하게해주는 부분
+app.get('/', (req, res) => res.send('Hello World! 블레ㅇ어입니다....')); // '/'인 즉 루트 디렉토리에 오면 헬로월드 출력하게해주는 부분
 
 app.post('/register', (req, res) => {
 
   // 회원가입시 필요한 정보들을 클라이언트에서 가져오면 그것을들 DB에 넣어준다.
-  const user = new User(req.body)
+  const user = new User(req.body);
 
   user.save((err,userInfo) => {
-    if(err) return res.json({ success: false, err })
+    if(err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true
     })
@@ -37,5 +38,5 @@ app.post('/register', (req, res) => {
 
 
 
-app.listen(port, () => console.log(`Example app listening on port${port}!!`))
+app.listen(port, () => console.log(`Example app listening on port${port}!!`));
 // 위에서 설정한 포트번호에서 이 app을 실행하기
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt'); // 다운받았던 bcrypt 가져오기
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -9,7 +10,11 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     trim: true,
-    unique: 5
+    unique: 1
+  },
+  password: {
+    type: Number,
+    minlength: 5
   },
   lastname: {
     type: String,
@@ -28,5 +33,25 @@ const userSchema = mongoose.Schema({
   }
 });
 
-const User = mongoose.model('User', userSchema)
-module.exports = { User }
+
+// restapi가 안되서 일단 주석처리
+// userSchema.pre('save', function (next) {
+//   const user = this;
+//
+//   if (user.isModified('password')) { // 비번이 수정될때만 암호화진행하
+//     // 비번 암호화시키기
+//     bcrypt.genSalt(saltRounds, function (err, salt) {
+//       if (err) return next(err);
+//
+//       bcrypt.hash(user.password, salt, function (err, hash) {
+//         if (err) return next(err);
+//         user.password = hash;
+//         next()
+//       })
+//     })
+//   }
+//
+// });
+
+const User = mongoose.model('User', userSchema);
+module.exports = {User};
